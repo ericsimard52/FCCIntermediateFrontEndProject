0afd9267517b6b4e1ad9e383e106097b8bf1c8b7
Eric Simard <esim1802@gmail.com>
2016-10-06 12:21:06 -0400

    Added fadeout and in effect on the quote, and attributed credits

--- QuoteMachine/js/scripts.js
+++ QuoteMachine/js/scripts.js
@@ -11,8 +11,14 @@ function quote(){
     //Methods
 
     GetQuote = function (){
-        $("#quoteRow").fadeOut("slow",function(){
+        if ($("#quoteContent").html().length > 0)
+            fadeOutQuote();
+        else
+            FetchQuote();
 
+    }
+
+    FetchQuote = function(){
         $.ajax({
             url:GetApi(),
             accept:"application/json" ,
@@ -27,18 +33,15 @@ function quote(){
             success:(function(data){
                 _quote = data.quote;
                 _quoteAuthor = data.author;
+
+                $("#tweetThis").attr("href","https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text="+_quote+" ---"+_quoteAuthor);
             }),
             complete:(function(){
-                $("#quoteContent").html(_quote);
+                fadeInQuote();
                 $("#quoteAuthor").html("--- "+_quoteAuthor);
-                $("#quoteRow").fadeIn("slow");
-                $("#tweetThis").attr("href","https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text="+_quote+" ---"+_quoteAuthor);
             })
             
         });
-            
-        });
-        /* Get a new random quotes */
     }
     
     SetApi = function(src){
@@ -51,6 +54,42 @@ function quote(){
         return _ApiSrc;
     }
 
+    fadeInQuote = function(){
+        //Fades in string letter by letter
+        $("#quoteContent").html("");
+        var str = _quote;
+        var strLength = str.length;
+        var x = 0;
+        var newStr= jQuery.map(str.split(''), function (letter) {
+            return $('<span id="lc'+x+++'">'+letter+'</span>');
+        });
+        var dest = $("#quoteContent");
+        var c = 0;
+        var i = setInterval(function () {
+            newStr[c].appendTo(dest).hide().fadeIn(200);
+            c += 1;
+            if (c >= newStr.length) clearInterval(i);
+        }, 50);
+
+    }
+
+    fadeOutQuote = function(){
+        //Fades out string letter by letter backwards
+        $("#quoteAuthor").html("");
+        var strLength = _quote.length;
+        var dest = $("#quoteContent");
+        var c = strLength - 1;
+        var i = setInterval(function () {
+            $("#lc"+c).fadeOut(50);
+            c--;
+            if (c < 0) {
+                clearInterval(i);
+                FetchQuote();
+            }
+        }, 10);
+
+    }
+    
     return {
         SetApi: SetApi,  //Return only what you want to use
         GetApi: GetApi,  //Everything else is "private"
@@ -58,12 +97,11 @@ function quote(){
     }
 };
 
-function testQuotes(){
-    /*Provides a series of test I perform against my quote object*/
-}
 //testQuotes();
 var myQuote = new quote();
 log(myQuote);
 myQuote.SetApi("https://andruxnet-random-famous-quotes.p.mashape.com/");
-$(document).ready(function(){myQuote.GetQuote()});
+$(document).ready(function(){
+    myQuote.GetQuote();
+});
 
